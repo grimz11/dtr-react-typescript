5bbc29a032f91ffd8d0c72277aa61a8d669945f8
Mark Andog <reymark@hyperstacksinc.com>
2021-02-23 16:11:01 +0800

    Mobx changed obervable decorator to makeAutoObservable

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -1,41 +1,42 @@
-import { action, observable } from "mobx";
+import { makeAutoObservable } from "mobx";
 import CurrentUserLogin from "../services/accountAuth/dto/currentUserLogin";
-import IUserOutput from "../services/user/dto/userOutput";
+// import IUserOutput from "../services/user/dto/userOutput";
 // import ILoginOutput from '../services/accountAuth/dto/loginOutput';
 import userService from "../services/user/userService";
 
 class UserStore {
-  @observable $currentLogin: any = new CurrentUserLogin();
-  @observable $allUsers: [] = [];
-  @observable $userProfile: [] = [];
-  @observable $userProfileEditable: [] = [];
+  $currentLogin: any = new CurrentUserLogin();
+  $allUsers: [] = [];
+  $userProfile: [] = [];
+  $userProfileEditable: [] = [];
 
-  @action
+  constructor() {
+    makeAutoObservable(this);
+  }
+
   async getCurrentLoginUser(id: number) {
     let result = await userService.getCurrentLoginUser(id);
     this.$currentLogin = result;
     return result;
   }
-  @action
+
   async getUserProfile(id: number) {
     let result = await userService.getUserProfile(id);
     this.$userProfile = result;
     return result;
   }
 
-  @action
   async getAllUsers() {
     let result = await userService.getAllUsers();
     this.$allUsers = result;
     return result;
   }
 
-  @action
   async updateUser(id: number, payload: any) {
     let result = await userService.updateUser(id, payload);
     return result;
   }
-  @action
+
   async uploadImage(formData: any) {
     let result = await userService.uploadImage(formData);
     return result;
